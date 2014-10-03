b4f873d8052c5eae4d776af14726021ec3331b94
UrbanBjorkman <urban.bjorkman@gmail.com>
2014-10-03 12:13:55 +0200

    added code

---
+++ background.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+    Date.prototype.getWeek = function () {
+        var onejan = new Date(this.getFullYear(), 0, 1);
+        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+    }
+
+
+    $(function () {
+        var weekNumber = (new Date()).getWeek().toString();
+        chrome.browserAction.setBadgeText({ text: weekNumber });
+    });
+})(jQuery);
